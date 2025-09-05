09fbae3f38ce7ca4b551d4d10bb72a9e239cec87
amit931063 <118598072+amit931063@users.noreply.github.com>
2025-09-05 21:30:04 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/NotesApp/MyNotesApp/security/WebSecurityConfig.java
+++ src/main/java/com/NotesApp/MyNotesApp/security/WebSecurityConfig.java
@@ -107,6 +107,7 @@ public class WebSecurityConfig {
 
             http
                     .csrf(csrf -> csrf.disable())
+                .cors(cors->{})
                     .authorizeHttpRequests(auth -> auth
                             .requestMatchers("/api/auth/**").permitAll()
                             .requestMatchers("/api/notes/**").permitAll()
